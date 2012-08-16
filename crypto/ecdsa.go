ff77a1c4710dc088735930bcf56a18477fdf1660
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2012-08-15 22:20:28 -0600

    Cleanup ecdsa example

--- crypto/ecdsa.go
+++ crypto/ecdsa.go
@@ -5,100 +5,45 @@ import (
     "crypto/elliptic"
     "crypto/rand"
     "crypto/sha1"
-    "encoding/gob"
     "flag"
     "io"
     "log"
-    "os"
 )
 
-const (
-    KeyFile = "ecdsa.key"
-)
-
-var (
-    message = flag.String("message", "Nuke the site from orbit, it's the only way to be sure.", "The message to sign")
-    do      = flag.String("do", "sign", "The operation to do, sign or verify")
-)
-
-func init() {
-    var key ecdsa.PrivateKey
-    gob.Register(key)
-
-    var cp elliptic.CurveParams
-    gob.Register(cp)
-
-    var c elliptic.Curve
-    gob.Register(c)
-}
+var message = flag.String("message", "Nuke the site from orbit, it's the only way to be sure.", "The message to sign")
 
 func HashMessage() []byte {
     h := sha1.New()
     _, err := io.WriteString(h, *message)
     if err != nil {
-        log.Fatalf("Failed to hash message: %s", err)
+        log.Fatalf("failed to hash message: %s", err)
     }
     return h.Sum(nil)
 }
 
-func MakeKey() *ecdsa.PrivateKey {
+func Key() *ecdsa.PrivateKey {
     key, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
     if err != nil {
-        log.Fatalf("Failed to generate key: %s", err)
+        log.Fatalf("failed to generate key: %s", err)
     }
     return key
 }
 
-func Key() *ecdsa.PrivateKey {
-    if file, err := os.Open(KeyFile); err == nil {
-        defer file.Close()
-        var key ecdsa.PrivateKey
-        dec := gob.NewDecoder(file)
-        err = dec.Decode(&key)
-        if err != nil {
-            log.Fatalf("Failed reading keyfile: %s", err)
-        }
-        return &key
-    } else {
-        log.Println("No keyfile, making a new one...")
-        key := MakeKey()
-        file, err = os.Create(KeyFile)
-        if err != nil {
-            log.Fatalf("Can't create file %s: %s", KeyFile, err)
-        }
-        defer file.Close()
-        enc := gob.NewEncoder(file)
-        err = enc.Encode(key)
-        if err != nil {
-            log.Fatalf("Failed saving key to %s: %s", KeyFile, err)
-        }
-        return key
-    }
-    panic("not reached")
-}
+func main() {
+    flag.Parse()
 
-func Sign() {
     key := Key()
     hash := HashMessage()
     r, s, err := ecdsa.Sign(rand.Reader, key, hash)
     if err != nil {
-        log.Fatalf("Failed to sign message: %s", err)
+        log.Fatalf("failed to sign message: %s", err)
     }
-    log.Println(r)
-    log.Println(s)
-}
+    log.Printf("r: %s", r)
+    log.Printf("s: %s", s)
 
-func Verify() {
-}
-
-func main() {
-    flag.Parse()
-    switch *do {
-    case "sign":
-        Sign()
-    case "verify":
-        Verify()
-    default:
-        log.Fatalf("%s is not a valid operation, must be one of sign or verify", *do)
+    if ecdsa.Verify(&key.PublicKey, hash, r, s) {
+        log.Println("message is valid!")
+    } else {
+        log.Println("message invalid :(")
     }
 }
